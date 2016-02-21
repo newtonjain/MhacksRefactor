24cc9bb1556df17701d8d98f3641a05989d76fdb
Angela Fox <angelafox92@gmail.com>
2016-02-21 04:03:02 -0500

    Added logic for Alexa responses

--- alexa/index.js
+++ alexa/index.js
@@ -64,8 +64,8 @@ MotivSkill.prototype.intentHandlers = {
 
 function getWelcomeResponse(response) {
     // If we wanted to initialize the session to have some attributes we could add those here.
-    var cardTitle = "Welcome to Motive";
-    var repromptText = "I can tell you what your goals are for the day, and keep you motivated. Ask me, What are my goals for today?";
+    var cardTitle = "Welcome to Motive, your very own personal motivation-donation system";
+    var repromptText =  "Ask me, What are my goals for today? Or, ask me, What are my achievements!";
     var speechText = "Welcome to Motive. I can tell you what your goals are for the day, and keep you motivated. Ask me, What are my goals for today?";
     var cardOutput = "I can tell you what your goals are for the day, and keep you motivated. Ask me, What are my goals for today?";
     // If the user either does not reply to the welcome message or says something that is not
@@ -80,6 +80,10 @@ function getWelcomeResponse(response) {
         type: AlexaSkill.speechOutputType.PLAIN_TEXT
     };
     response.askWithCard(speechOutput, repromptOutput, cardTitle, cardOutput);
+
+    console.log('Loading function');
+
+
 }
 
 /**
@@ -94,8 +98,16 @@ function goalIntentRequest(intent, session, response) {
         console.log(events[2]);
         console.log(events[4]);
 
-        var speech = "Goals " + events[0] + events[2] + events[4];
-        response.askWithCard(speech, "", "card");
+        var speech = "Here are your goals for today: "
+             + "Your first goal is. " + events[0] 
+             + " " 
+             + "Your second goal is. " + events[2]
+             + " " 
+             +" and your third goal is. " + events[4]
+             +" "
+             +"Have a wonderful day!";
+
+        response.askWithCard(speech, "Goals for the Day", "Goal One: " + events[0] + "\nGoal Two: " + events[2] + "\nGoal Three: " + events[4]);
         return;
     });
 }
@@ -105,16 +117,49 @@ function achievementIntentRequest(intent, session, response) {
     
     getJsonEventsFromWikipedia(function (events) {
         
+        var speech = "";
+        var goalFail = 0;
 
-        console.log(events[0]);
-        console.log(events[1]);
-        console.log(events[2]);
-        console.log(events[3]);
-        console.log(events[4]);
-        console.log(events[5]);
+        //CHECK THIS
+        if (events[1] == "False") {
+            goalFail += 1;
 
-        var speech = "Goals " + events[0] + events[1] + events[2] + events[3] + events[4]+ events[5] ;
-        response.askWithCard(speech, "", "card");
+        }
+        if (events[3]) == "False") {
+            goalFail += 3;
+        }
+        if (events[5]) == "False") {
+            goalFail += 5;
+        }
+
+        if (goalFail == 0) {
+            speech = "Amazing work today! You completed all of your goals!";
+        }
+
+        var oneSpeech = "Great work! You completed two out of three goals. Tommorow, I'm certain that you will make time to ";
+
+        if (goalFail < 4) {
+            speech = oneSpeech + events[goalFail];
+        }
+
+        var twoSpeech = "Let's do better tomorrow! You did not complete two of your goals, to:  ";
+        var twoSpeechctd = ". Nonetheless, I'm proud of you for prioritizing your goal to: ";
+            
+        if (goalFail == 4) {
+            speech = twoSpeech + events[1] + "and" + events[3] + twoSpeechctd + events[5];
+        } else if { (goalFail == 6) {
+            speech = twoSpeech + events[1] + "and" + events[5] + twoSpeechctd + events[3];
+        } else {
+            speech = twoSpeech + events[3] + "and" + events[5] + twoSpeechctd + events[1];
+        }
+
+        if (goalFail == 9) {
+            speech = "We all have days that we miss the mark."
+                + " You may not have completed your goals, "
+                + "but you'll be making a generous donation to YOUR CHARITY HERE";
+        }
+
+        response.askWithCard(speech, "Achievements", "Achievements");
         return;
     });
 }
@@ -155,9 +200,28 @@ function parseJson(inputText) {
     return retArr;
 }
 
+
+ 
+
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
     // Create an instance of the Motiv Skill.
     var skill = new MotivSkill();
     skill.execute(event, context);
+
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('value1 =', event.key1);
+    console.log('value2 =', event.key2);
+    console.log('value3 =', event.key3);
+    console.log('remaining time =', context.getRemainingTimeInMillis());
+    console.log('functionName =', context.functionName);
+    console.log('AWSrequestID =', context.awsRequestId);
+    console.log('logGroupName =', context.logGroupName);
+    console.log('logStreamName =', context.logStreamName);
+    console.log('clientContext =', context.clientContext);
+    if (typeof context.identity !== 'undefined') {
+        console.log('Cognity identity ID =', context.identity.cognitoIdentityId);
+    }    
+    context.succeed(event.key1);  // Echo back the first key value
+    // context.fail('Something went wrong');
 };
